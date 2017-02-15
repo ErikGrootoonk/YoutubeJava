b27939af0a13d4a429b668efc519f6e0a91d3b21
Erik Grootoonk <erik.grootoonk@gmail.com>
2017-02-15 19:15:36 +0100

    project setup

---
+++ src/homeDir.java
@@ -0,0 +1,9 @@
+/**
+ * Created by erikg on 13/02/2017.
+ */
+class HomeDirEx {
+        public static void main(String[] args) {
+            System.out.println(System.getProperty("user.home"));
+        }
+    }
+
